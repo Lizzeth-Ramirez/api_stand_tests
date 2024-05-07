0c354a4839d9942371d5fe689144227d56f0bc8b
Lizzeth Ramirez <jlizz.r@gmail.com>
2024-05-06 19:18:02 -0600

    Mi primer commit

---
+++ api_stand_tests/configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = " https://cnt-2a1ca150-38c1-4158-b121-cf0434366ff8.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
